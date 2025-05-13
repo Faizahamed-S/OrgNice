27eed32277db72c1f4a36f2b3f0cb4d6e49555cb
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2025-05-13 11:12:12 -0700

    Install tailwind + react
    checking whether tailwind is working or not

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-600 text-white text-4xl font-bold">
+      Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
+
