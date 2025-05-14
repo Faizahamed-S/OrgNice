1603e00e5ce09f57dfc1287fe2360fa3334fde91
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2025-05-13 20:08:44 -0700

    Install typescript

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.log('❌ MongoDB error:', err));
